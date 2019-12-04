2345062485838028501a6597fdd7406a256b2d2e
daylanvig <daylanvig@gmail.com>
2019-12-04 16:01:53 -0700

    Init commit

---
+++ FinanceApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using FinanceApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
